76bc9c28c87c4a373130d941ba8c938d60a41501
zengyu <1796587140@qq.com>
2026-06-04 16:11:19 +0800

    refactor(cesium-web): 移除未使用的插件系统
    移除了应用程序中未实际使用的插件初始化代码和空的插件模块，
    简化了应用启动流程并减少不必要的依赖引入

--- apps/cesium-web/src/main.tsx
+++ apps/cesium-web/src/main.tsx
@@ -4,11 +4,6 @@ import "./styles/index.css";
 import App from "./App.tsx";
 import { ThemeProvider } from "./contexts/ThemeContext";
 
-import plugins from "./plugins/index.ts";
-
-// 初始化应用插件
-plugins.install();
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
